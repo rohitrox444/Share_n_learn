3036fb99b523687e722f15889af02307774a773c
rohitrox444 <rohitrox444@gmail.com>
2020-06-11 20:10:12 +0530

    Mysql Migration Established

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class blog_list(models.Model):
+
+    blog_name = models.CharField(max_length=100)
+    blog_description = models.CharField(max_length=100)
+    
+
+    class Meta:
+        db_table = "blog_list"
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from blog.models import blog_list
 # Create your views here.
+
+def showBlogs(request):
+    blog_list_data = blog_list.objects.all()
+    return render(request,'blog-page.html',{'blog_list_data':blog_list_data})
